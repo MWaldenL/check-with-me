58b37a778004c345d7b40e3d7d27524503d767df
Matthew Lua <matthew_walden_lua@dlsu.edu.ph>
2021-01-06 22:43:07 +0800

    Add persisted state  package

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vuex from 'vuex'
 import cells from './modules/cells/index.js'
 import auth from './modules/auth/index.js'
 import game from './modules/game/index.js'
+import createPersistedState from 'vuex-persistedstate'
 
 // load vuex
 Vue.use(Vuex)
 
 // create store
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   modules: {
     cells,
     auth,
